cb4949bead300fe05b8a3e8755bebdeba9a123dd
Ersin <113672117+ErsinCabuk@users.noreply.github.com>
2024-01-08 18:31:09 +0300

    added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ECBuilderGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
